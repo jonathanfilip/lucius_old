d2b664f1f12f118cff6fbd5ac2b22c0d81aef643
Jonathan Filip <jfilip1024@gmail.com>
2011-08-08 22:28:35 -0500

    updates

--- bashfiles/aliases.sh
+++ bashfiles/aliases.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-alias ls='ls --color -F'
 alias lt='ls -ltrF'
 alias lta='ls -ltrAF'
 alias ll='ls -lF'
 alias lsl='ls -lF'
 alias lla='ls -lAF'
+
+if [[ $platform == 'linux' ]]; then
+    alias ls='ls --color=auto'
+    #alias ls='ls --color -F'
+elif [[ $platform == 'osx' ]]; then
+    alias ls='ls -G'
+fi
+
