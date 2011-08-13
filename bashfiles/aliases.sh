553441c6c0f8070457011682e94596437620775c
Jonathan Filip <jfilip1024@gmail.com>
2011-08-12 19:27:51 -0500

    More updates

--- bashfiles/aliases.sh
+++ bashfiles/aliases.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-alias lt='ls -ltrF'
-alias lta='ls -ltrAF'
-alias ll='ls -lF'
-alias lsl='ls -lF'
-alias lla='ls -lAF'
-
+# ls Commands
 if [[ $platform == 'linux' ]]; then
     alias ls='ls --color=auto'
     #alias ls='ls --color -F'
 elif [[ $platform == 'osx' ]]; then
     alias ls='ls -G'
 fi
+alias lt='ls -ltr'
+alias lta='ls -ltrA'
+alias ll='ls -l'
+alias lla='ls -lA'
+
+# Navigation
+alias up="cd ..; "
+alias ..="cd ..; "
+
+# Program shortcuts
+alias vi=vim
+alias l="less"
+
+# Other Commands
+alias psme='ps -Af | grep ${USER}'
+alias dfme='df -h | egrep "${USER}|Filesystem"'
+alias xterm='xterm -ls' # Always do login shell
 
--- bashfiles/bashrc.sh
+++ bashfiles/bashrc.sh
@@ -21,3 +21,53 @@ export EDITOR=vim
 export VISUAL=vim
 export IGNOREEOF=1 # press ctrl+D twice to exit
 export PROMPT_COMMAND=bash_prompt # Set up the command line
+
+
+function bash_prompt()
+{
+    local NONE="\[\033[0m\]"
+
+    local K="\[\033[0;30m\]"    # black
+    local R="\[\033[0;31m\]"    # red
+    local G="\[\033[0;32m\]"    # green
+    local Y="\[\033[0;33m\]"    # yellow
+    local B="\[\033[0;34m\]"    # blue
+    local M="\[\033[0;35m\]"    # magenta
+    local C="\[\033[0;36m\]"    # cyan
+    local W="\[\033[0;37m\]"    # white
+
+    # empahsized (bolded) colors
+    local EMK="\[\033[1;30m\]"    # black  
+    local EMR="\[\033[1;31m\]"    # red    
+    local EMG="\[\033[1;32m\]"    # green  
+    local EMY="\[\033[1;33m\]"    # yellow 
+    local EMB="\[\033[1;34m\]"    # blue   
+    local EMM="\[\033[1;35m\]"    # magenta
+    local EMC="\[\033[1;36m\]"    # cyan   
+    local EMW="\[\033[1;37m\]"    # white  
+
+    # background colors
+    local BGK="\[\033[40m\]"    # black  
+    local BGR="\[\033[41m\]"    # red    
+    local BGG="\[\033[42m\]"    # green  
+    local BGY="\[\033[43m\]"    # yellow 
+    local BGB="\[\033[44m\]"    # blue   
+    local BGM="\[\033[45m\]"    # magenta
+    local BGC="\[\033[46m\]"    # cyan   
+    local BGW="\[\033[47m\]"    # white  
+
+    if [ "${SHELL_TAG}" ]; then
+        PS1="  ${EMK}[${NONE}${Y}${SHELL_TAG}${NONE}${EMK}]${NONE} ${EMK}[${NONE}${C}\u@\h${NONE}${EMK}]${NONE} ${G}\w${NONE}\n> "
+    else
+        PS1="  ${EMK}[${NONE}${C}\u@\h${NONE}${EMK}]${NONE} ${G}\w${NONE}\n> "
+    fi
+
+    if [ "${TITLE}" ]; then
+        echo -ne "\033]0;${TITLE} - ${HOSTNAME}\007"
+    else
+        echo -ne "\033]0;${HOSTNAME}\007"
+    fi
+}
+
+set umask 0002
+
