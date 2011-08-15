8349e6b3c0cf45c7a8087836f9051c9b3f214d7c
Jonathan Filip <jfilip1024@gmail.com>
2011-08-14 22:48:43 -0500

    Removed cig

--- bashfiles/cig/bashrc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export LS_COLORS="no=00:fi=00:di=00;94:ln=00;96:pi=40;33:so=00;95:bd=40;33;00:cd=40;33;00:or=00;05;37;41:mi=00;05;37;41:ex=00;92:ow=00;94" # LS Colors for Linux
-
-export DISPLAY=chfilip01:0.0
-
-export GIT_SSL_NO_VERIFY=true
-
